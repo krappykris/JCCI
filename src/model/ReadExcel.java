18b80cf9ddeff07ab5c3d4ec69157254295ccdf8
Kris Kristofferson Casio Nabua <kingkris112@gmail.com>
2014-07-02 10:38:50 +0800

    1st Update
    Can choose file to import as csv and can handle the data in the from the
    csv file. :)

--- src/model/ReadExcel.java
+++ src/model/ReadExcel.java
@@ -1,21 +1,36 @@
 package model;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.JFileChooser;
+
 
 public class ReadExcel {
-	String csvToRead = null;
+//	String csvToRead = null;
+	
 	BufferedReader buffRead = null;
 	String line = null;
 	String splitBy = ",";
 	String[] data = null;
 	
 	public ReadExcel() throws IOException{
-		csvToRead = "/Users/Kris/Documents/carmel.csv";
+	importCSVFile();	
+	}
+	
+	public void importCSVFile() throws IOException
+	{	
+		JFileChooser chooser = new JFileChooser();
+		int returnVal = chooser.showOpenDialog(null);
+		File file = null;
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+		file = chooser.getSelectedFile(); // path to image
+		}
+		
 		try {
-			buffRead = new BufferedReader(new FileReader(csvToRead));
+			buffRead = new BufferedReader(new FileReader(file));
 			while((line = buffRead.readLine())!=null){
 				data = line.split(splitBy);
 				if(data.length != 0){
@@ -28,6 +43,7 @@ public class ReadExcel {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 	public static void main(String args[]) throws IOException{
